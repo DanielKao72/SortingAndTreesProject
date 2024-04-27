620d1ef249657417103463a64898dbc126d832f6
Juan Daniel Kao Pech <a19201340@alumnos.uady.mx>
2024-04-27 13:48:10 -0600

    Modificación Método de Tiempo de Ejecución

--- src/main/java/main/Utilidades.java
+++ src/main/java/main/Utilidades.java
@@ -29,22 +29,12 @@ public ArrayList<String> tokenizador(String texto) {
         return arregloPalabras;
     }
 
-    public ArrayList<String> medirTiempoejecucionOrdenamiento(ArrayList<String> data, String methodName, Consumer<ArrayList<String>> operation) {
+    public long medirTiempoejecucionOrdenamiento(ArrayList<String> data, String methodName, Consumer<ArrayList<String>> operation) {
         long start = System.currentTimeMillis();
         operation.accept(data);
         long finish = System.currentTimeMillis();
         long timeElapsed = finish - start;
-        System.out.println("El tiempo total para " + methodName + " es de " + timeElapsed);
-        return data;
-    }
-
-    public ArrayList<String> medirTiempoejecucionOrdenamientoConRetorno(ArrayList<String> data, String methodName, Function<ArrayList<String>, ArrayList<String>> operation) {
-        long start = System.currentTimeMillis();
-        ArrayList<String> result = operation.apply(data);
-        long finish = System.currentTimeMillis();
-        long timeElapsed = finish - start;
-        System.out.println("El tiempo total para " + methodName + " es de " + timeElapsed);
-        return result;
+        return timeElapsed;
     }
 
     public Boolean elElementoSeEncuentra(ArrayList<String> arregloOrdenado, String elementoBuscado) {
